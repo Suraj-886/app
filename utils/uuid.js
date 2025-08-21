053a23c0c7e7e502e3ec9b21474fcfe80431da52
Suraj kumar <Suraj2201kumar@gmail.com>
2025-08-21 11:51:14 +0530

    change MSMI folder utils

--- utils/uuid.js
+++ utils/uuid.js
@@ -1,6 +1,6 @@
 function generateMSMENumber() {
   const digits = Array.from({ length: 8 }, () => Math.floor(Math.random() * 10)).join('');
-  return `MSME-${digits}`;
+  return digits;
 }
 
 module.exports = generateMSMENumber;
